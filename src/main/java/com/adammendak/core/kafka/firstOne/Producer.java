1a2f64c569b5fefaf2ffe3a29ac400d097fed082
adammendak <adam.mendak@gmail.com>
2018-12-10 20:53:15 +0100

    kafka

---
+++ src/main/java/com/adammendak/core/kafka/firstOne/Producer.java
@@ -0,0 +1,14 @@
+package com.adammendak.core.kafka.firstOne;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+public class Producer {
+
+    Properties properties = new Properties();
+
+
+
+}
